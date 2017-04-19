96a95b8fb7b5fa772fcea423ceb7cecd40d6ea9f
rake146 <jake.pinchbeck@hotmail.co.uk>
2017-04-19 23:36:39 +0100

    Setting up server
    Setting up server

--- server.js
+++ server.js
@@ -1,1 +1,55 @@
+var blobs = [];
+
+function Blob(id, x, y, r){
+  this.id = id;
+  this.x = x;
+  this.y = y;
+  this.r = r;
+}
+var express = require('express');
+var app = express();
+var server = app.listen(3000);
+
+app.use(express.static('public'));
 console.log("MY SOCKET SERVER IS RUNNING");
+
+var socket = require('socket.io');
+
+var io = socket(server);
+
+setInterval(heartbeat, 1000);
+
+function heartbeat(){
+  io.emit('heartbeat', blobs);
+}
+io.sockets.on('connection',
+//io.sockets.on('connection', newConnection);
+  function (socket){
+    socket.on('start', function start(data){
+      console.log(socket.id + " " + data.x + " " + data.y + " " + data.r);
+      //io.sockets.emit('mouse', data);
+      var blob = new Blob(socket.id, data.x, data.y, data.r);
+      blobs.push(blob);
+      //console.log(data);
+    });
+
+    socket.on('update', function(data){
+      console.log(socket.id + " " + data.x + " " + data.y + " " + data.r);
+      //io.sockets.emit('mouse', data);
+      var blob;
+
+      for (var i = 0; i < blobs.length; i++) {
+          if (socket.id == blobs[i].id){
+            blob = blobs[i];
+          }
+      }
+
+      //blob.x = data.x;
+      //blob.y = data.y;
+      //blob.r = data.r;
+
+      //blobs.push(blob);
+      //console.log(data);
+    });
+  }
+)
